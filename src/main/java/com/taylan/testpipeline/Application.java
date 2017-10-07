99c93d28b55737f374a60f1564f117ab75c966b1
Taylan Kurt <taylan.kurt@iyzico.com>
2017-10-07 14:19:17 +0300

    Walking skeleton

---
+++ src/main/java/com/taylan/testpipeline/Application.java
@@ -0,0 +1,12 @@
+package com.taylan.testpipeline;
+
+public class Application {
+
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    public void doSomeStupid() {
+        System.err.println("blabalbalbablabl");
+    }
+}
---
+++ src/test/java/com/taylan/testpipeline/ApplicationTest.java
@@ -0,0 +1,40 @@
+package com.taylan.testpipeline;
+
+import com.taylan.testpipeline.testconfig.extension.MockitoExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class ApplicationTest {
+
+
+    @Test
+    void should_say_hello_to_world() {
+        //given
+        Application application = new Application();
+
+        //when
+        String hello = application.sayHello();
+
+        //then
+        assertThat(hello).isEqualTo("Hello World!");
+    }
+
+    @Test
+    void should_do_something_stupid() {
+        //given
+        Application mockApplication = Mockito.mock(Application.class);
+
+        //when
+        mockApplication.doSomeStupid();
+
+        //then
+        InOrder inOrder = Mockito.inOrder(mockApplication);
+        inOrder.verify(mockApplication).doSomeStupid();
+        inOrder.verifyNoMoreInteractions();
+    }
+}
